b310033b2a4ea5efcddec7b82684161da745e1ac
bashimovich <yagshy.script@gmail.com>
2024-09-23 04:41:06 +0500

    video player loading..

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -39,12 +39,6 @@ function VideoPlayer() {
           console.log(err);
         });
   }, [key])
-    if (!currentVideo) {
-        return <div>Loading...</div>;
-    }
-    if (!RandomVideo) {
-        return <div>Loading...</div>;
-    }
 
   return (
     <>
