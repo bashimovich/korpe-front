4ff1fa51f49b7ab644b86a3794b5eb17b1973ddc
bashimovich <yagshy.script@gmail.com>
2024-09-26 07:57:08 +0500

    added teachers and parents pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './../assets/images/korpe-logo.png'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
-
-
-
 function Navbar() {
     const location = useLocation();
     const currentPath = location.pathname;
+    console.log(currentPath);
+    
     const {t, i18n} = useTranslation()
     const navigate = useNavigate()
     const [sidetoggle, setsidetoggle] = useState(false)
     function goHome() {
        navigate('/') 
+    }
+    function goKids() {
+       navigate('/') 
+    }
+    function goTeachers() {
+        navigate('/teachers') 
+    }
+    function goParents() {
+        navigate('/parents') 
+    }
+    function goAbout() {
+        navigate('/about') 
     }
     function changeLanguage(lang) {
        i18n.changeLanguage(lang)
@@ -22,7 +33,7 @@ function Navbar() {
     }
   return (
     <>
-        <div className="navbar bg-green-400 bottom-box-shadow sticky top-0">
+        <div className="navbar bg-green-400 bottom-box-shadow sticky top-0 z-50">
             <div className="container flex items-center justify-between">
                 <div onClick={goHome} className="logo hover:cursor-pointer flex items-center justify-start pt-2 pb-2 bg-green-400">
                     <img src={logo} className='w-[70px]' alt="" />
@@ -34,11 +45,11 @@ function Navbar() {
                         <div className='bg-white w-8 h-1 mt-2 rounded-md'></div>
                         <div className='bg-white w-8 h-1 mt-2 rounded-md'></div>
                     </div>
-                    <div className={`absolute bg-green-400 top-16 right-0 left-0 overflow-hidden transition-all duration-800 ${sidetoggle ? 'h-screen  pt-5':'h-0'}`}>
-                    <p className={`${currentPath.startsWith('') || currentPath.startsWith('/kids/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("babies")}</p>
-                    <p className={`${currentPath.startsWith('/parents/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("parents")}</p>
-                    <p className={`${currentPath.startsWith('/teachers/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t('teachers')}</p>
-                    <p className={`${currentPath.startsWith('/aboutus/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("aboutus")}</p>
+                    <div className={`absolute bg-green-400 top-28 right-0 left-0 overflow-hidden transition-all duration-800 ${sidetoggle ? 'h-screen  pt-5':'h-0'}`}>
+                    <p onClick={goKids} className={`${currentPath ==='/' || currentPath.startsWith('/kids') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("babies")}</p>
+                    <p onClick={goParents} className={`${currentPath.startsWith('/parents/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("parents")}</p>
+                    <p onClick={goTeachers} className={`${currentPath.startsWith('/teachers') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t('teachers')}</p>
+                    <p onClick={goAbout} className={`${currentPath.startsWith('/about/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("aboutus")}</p>
                         <p className='mb-3 p-0 m-0 ml-5 text-white font-bold text-xl'>
                             <span onClick={() => {changeLanguage('tm')}} className={`${i18n.language === 'tm' ? 'text-green-700':''} ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>TM</span>
                             <span className='ml-1 mr-1'>|</span>
@@ -51,10 +62,10 @@ function Navbar() {
                 </div>
 
                 <div className='hidden md:flex items-center justify-end'>
-                    <p className={`${currentPath.startsWith('') || currentPath.startsWith('/kids/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("babies")}</p>
-                    <p className={`${currentPath.startsWith('/parents/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("parents")}</p>
-                    <p className={`${currentPath.startsWith('/teachers/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t('teachers')}</p>
-                    <p className={`${currentPath.startsWith('/aboutus/') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 text-white font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("aboutus")}</p>
+                    <p onClick={goKids} className={`${currentPath === '/' || currentPath.startsWith('/kids') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("babies")}</p>
+                    <p onClick={goParents} className={`${currentPath.startsWith('/parents') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("parents")}</p>
+                    <p onClick={goTeachers} className={`${currentPath.startsWith('/teachers') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t('teachers')}</p>
+                    <p onClick={goAbout} className={`${currentPath.startsWith('/about') ? 'text-green-700':'text-white'} p-0 m-0 ml-5 font-bold text-xl ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>{t("aboutus")}</p>
                     <p className='p-0 m-0 ml-5 text-white font-bold text-xl'>
                         <span onClick={() => {changeLanguage('tm')}} className={`${i18n.language === 'tm' ? 'text-green-700':''} ease-in duration-300 hover:text-green-700 hover:cursor-pointer`}>TM</span>
                         <span className='ml-1 mr-1'>|</span>
---
+++ src/pages/ReadParents.jsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import { axiosInstance, BASE_DOMAIN } from '../utils/axiosInstance';
+import { useTranslation } from 'react-i18next';
+import { formatDistanceToNow } from 'date-fns';
+import { useNavigate, useParams } from 'react-router-dom';
+import { SmalltruncateText, truncateBigText, truncateText } from '../utils/maxCharacter';
+import i from './../assets/images/korpe-logo.png'
+import ii from './../assets/images/tomandjerry.jpg'
+
+function ReadParents() {
+    const {t, i18n} = useTranslation()
+    const timeAgo = (dateString) => {
+        return formatDistanceToNow(new Date(dateString), { addSuffix: true });
+    };
+    const { key } = useParams();  
+    const [currentBlog, setcurrentBlog] = useState()
+    const navigate = useNavigate()
+    const [RandomVideo, setRandomVideo] = useState()
+    function gotowatch(key) {
+        navigate(`/kids/video/${key}`)
+    }
+    useEffect(() => {
+    window.scroll(0,0)
+      axiosInstance
+        .get(`videos/${key}/`)
+        .then((res) => {
+            setcurrentBlog(res.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+        axiosInstance
+            .get(`videos/`)
+            .then((res) => {
+                setRandomVideo(res?.data.results)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+  }, [key])
+    if (!currentBlog) {
+        return <div>Loading...</div>;
+    }
+  return (
+    <>
+        <Navbar />
+        <div class="bg-gradient-to-r from-green-400 to-blue-500">
+            <div className="video_list">
+                <div className="container">
+                    {/* <div className='pt-5'>
+                        <input className='w-full md:w-1/2 p-2 border-l-emerald-400 focus:outline-none text-xl rounded-3xl pl-5 pr-5' type="text" />
+                    </div> */}
+                    <div className='p-2 md:p-20 w-full md:w-[80%] m-auto'>
+                        <div className='flex items-center justify-between pt-5 pb-5'>
+                            <div className='flex justify-start items-center'>
+                                <img className='w-14 h-14 rounded-full mr-2' src={i} alt="" />
+                                <p className='text-white text-lg'>yagshy_ashyrov</p>
+                            </div>
+                            <div>
+                                <p className='text-left text-white text-sm'>10 views - 10 days ago</p>
+                            </div>
+                        </div>
+                        <h1 className='text-left text-white text-3xl font-semibold mb-4 mt-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, iste non! Nobis officiis nostrum repudiandae. Odit sunt nisi deleniti maxime?</h1>
+                        <img src={ii} className='w-full m-auto' alt="" />
+                        <p className='text-lg text-justify mt-4 text-white'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque consequatur perferendis iure, accusamus molestiae earum, culpa corporis laborum asperiores tempore iusto cumque facere possimus id dolor sequi eum exercitationem expedita.
+                        </p>
+                    </div>
+                    <div className='border-t-2 pt-10 grid gap-1 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 lg:gap-2'>
+                        <div className='w-full ease-in duration-300 hover:bg-green-300 hover:cursor-pointer p-3 rounded-xl'>
+                            <img src={ii} className='w-full h-52 rounded-lg' alt="" />
+                            <div className='flex p-3 items-start'>
+                                <div className='flex items-center justify-center mr-2'>
+                                    <img src={i} className='min-w-10 w-10 h-10' alt="" />
+                                </div>
+                                <div>
+                                    <p className='text-white text-left'>yagshy</p>
+                                    <p className='text-white text-left'>10 views - 10 days ago</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-full ease-in duration-300 hover:bg-green-300 hover:cursor-pointer p-3 rounded-xl'>
+                            <img src={ii} className='w-full h-52 rounded-lg' alt="" />
+                            <div className='flex p-3 items-start'>
+                                <div className='flex items-center justify-center mr-2'>
+                                    <img src={i} className='min-w-10 w-10 h-10' alt="" />
+                                </div>
+                                <div>
+                                    <p className='text-white text-left'>yagshy</p>
+                                    <p className='text-white text-left'>10 views - 10 days ago</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-full ease-in duration-300 hover:bg-green-300 hover:cursor-pointer p-3 rounded-xl'>
+                            <img src={ii} className='w-full h-52 rounded-lg' alt="" />
+                            <div className='flex p-3 items-start'>
+                                <div className='flex items-center justify-center mr-2'>
+                                    <img src={i} className='min-w-10 w-10 h-10' alt="" />
+                                </div>
+                                <div>
+                                    <p className='text-white text-left'>yagshy</p>
+                                    <p className='text-white text-left'>10 views - 10 days ago</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-full ease-in duration-300 hover:bg-green-300 hover:cursor-pointer p-3 rounded-xl'>
+                            <img src={ii} className='w-full h-52 rounded-lg' alt="" />
+                            <div className='flex p-3 items-start'>
+                                <div className='flex items-center justify-center mr-2'>
+                                    <img src={i} className='min-w-10 w-10 h-10' alt="" />
+                                </div>
+                                <div>
+                                    <p className='text-white text-left'>yagshy</p>
+                                    <p className='text-white text-left'>10 views - 10 days ago</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Footer />
+    </>
+  )
+}
+
+export default ReadParents
