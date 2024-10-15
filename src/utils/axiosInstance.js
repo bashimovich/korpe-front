f7138b5b93b48e610ebc423902bd09e41bac6ce7
bashimovich <yagshy.script@gmail.com>
2024-10-15 10:35:07 +0500

    axios change

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,11 +2,11 @@ import axios from "axios";
 // import { token } from "./token";
 
 
-// const BASE_URL = "https://gel.edu.tm:8000/api/v1/";
-// const BASE_DOMAIN = "https://gel.edu.tm:8000/";
+const BASE_URL = "https://gel.edu.tm:8000/api/v1/";
+const BASE_DOMAIN = "https://gel.edu.tm:8000/";
 
-const BASE_URL = "http://localhost:8000/api/v1/";
-const BASE_DOMAIN = "http://localhost:8000/";
+// const BASE_URL = "http://localhost:8000/api/v1/";
+// const BASE_DOMAIN = "http://localhost:8000/";
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
     timeout: 10000,
